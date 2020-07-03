ff51a8649086d1bf815c5ea69aff5a07e99af7de
myfix16 <myfix16@outlook.com>
2020-07-03 11:02:05 +0800

    添加项目文件。

---
+++ BindingFunctions.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace AutoComplete
+{
+    public class BindingFunctions
+    {
+        [DllImport("user32.dll")]
+        static extern short GetKeyState(int nVirtKey);
+
+        public static bool IsKeyDown(Keys keys)
+        {
+            return (GetKeyState((int)keys) & 0x8000) == 0x8000;
+        }
+
+    }
+}
